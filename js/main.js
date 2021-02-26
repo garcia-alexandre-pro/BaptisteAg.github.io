e8a93848e28ea7308c1b98e214269c0bd93c068e
w4lexs <45164638+w4lexs@users.noreply.github.com>
2021-02-26 12:49:59 +0100

    talents

--- js/main.js
+++ js/main.js
@@ -56,29 +56,3 @@ checkbox.addEventListener('change', function () {
         window.scrollTo(0, parseInt(scrollY || '0') * -1);
     }
 });
-
-var lang = {
-    "html": "100%",
-    "css": "90%",
-    "javascript": "70%",
-    "php": "55%",
-    "angular": "65%"
-};
-
-var multiply = 4;
-
-// $.each(lang, function (language, pourcent) {
-
-//     var delay = 700;
-
-//     setTimeout(function () {
-//         $('#' + language + '-pourcent').html(pourcent);
-//     }, delay * multiply);
-
-//     multiply++;
-
-// });
-lang.forEach((item, index) => {
-    console.log(item)
-    console.log(index)
-});
