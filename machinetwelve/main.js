354f9e5d3689ea7d78a8618a6391baff251812c7
w4lexs <45164638+BaptisteAg@users.noreply.github.com>
2021-05-21 10:46:01 +0200

    2.3

--- machinetwelve/main.js
+++ machinetwelve/main.js
@@ -17,10 +17,10 @@ var LinkSection = new Vue({
                         if (data.data !== undefined) {
                             vm.Message = 'OK'
                             DownloadList.Message = data.data.filename
+                            StreamingList.FileName = data.data.filename
                             DownloadList.Link = data.data.link
                             DownloadList.LinkReceived = true
                             if (data.data.streams !== undefined) {
-                                StreamingList.VisiblePlayer = false
                                 StreamingList.Message = "Qualité et langue (qual-lang) :"
                                 StreamingList.QualitysList = []
                                 StreamingList.QualitysList.push(data.data.streams)
@@ -57,6 +57,7 @@ var StreamingList = new Vue({
         Message: 'La machine n\'a pas encore fonctionné...',
         Id: null,
         LinkClear: null,
+        FileName: null,
         QualitysList: [],
         SubtitlesList: [],
         StreamQualityReceived: false,
@@ -67,15 +68,15 @@ var StreamingList = new Vue({
             $.getJSON(
                 'https://api.alldebrid.com/v4/link/streaming?agent=LinkOk&apikey=yzDd2zmszOk2lDbxsyeb&id=' + StreamingList.Id + '&stream=' + $("#streamqual").val(),
                 function (data) {
-                    if (typeof videoplayer === 'undefined') {
+                    if (!StreamingList.VisiblePlayer) {
                         StreamingList.LinkClear = data.data.link
                         StreamingList.VisiblePlayer = true
                     } else {
-                        videoplayer.src({ type: "video/mp4", src: data.data.link });
+                        videojs('videoplayerid').src({ type: "video/mp4", src: data.data.link });
                     }
                     StreamingList.StreamQualityReceived = false
                     DownloadList.LinkReceived = false
-                    StreamingList.Message = 'Pour changer de qualité/changer d\'épisode, la page doit étre rechargé (Réglé dans la prochaine maj).'
+                    StreamingList.Message = 'Pour changer de qualité/changer d\'épisode, le lien doit être re générer.'
                     DownloadList.Message = 'Pour télécharger le ficher, le lien doit être re générer.'
                     $("#streamingList").css("width", "95vw")
                     document.querySelector('#streamingList').scrollIntoView({
@@ -85,10 +86,10 @@ var StreamingList = new Vue({
             )
         }
     },
-    updated: function () {
-        if (typeof videoplayer === 'undefined') {
+    watch: {
+        VisiblePlayer: function (val) {
             this.$nextTick(function () {
-                var videoplayer = videojs('videoplayerid').ready(function () {
+                videojs('videoplayerid').ready(function () {
                     this.hotkeys({
                         volumeStep: 0.1,
                         seekStep: 5,
